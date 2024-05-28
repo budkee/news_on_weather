8ac3c000fb3aa588d2051c0b61406012dd484d4e
budkee <kae.budke@gmail.com>
2024-05-27 23:02:47 -0400

    Alteração para OO

--- coleta_armazena.py
+++ coleta_armazena.py
@@ -1,8 +1,9 @@
 import argparse
 import requests
-import threading
 import pandas as pd
 import mysql.connector
+import time
+import random
 
 class SingletonMeta(type):
     """
@@ -11,22 +12,15 @@ class SingletonMeta(type):
     _instances = {}
     _lock: threading.Lock = threading.Lock()
 
-    # Cria ou retorna a instância de uma determinada classe a partir do dicionário de instâncias
     def __call__(cls, *args, **kwargs):
-        # Bloqueia a execução da thread até que seja executado o bloco de código
         with cls._lock:
-            # Caso a classe não esteja no dicionário
             if cls not in cls._instances:
-                # Crie uma nova instância a partir do construtor
                 instance = super().__call__(*args, **kwargs)
-                # Armazena a nova instância no dicionário
                 cls._instances[cls] = instance
-        # Retorna a instância armazenado no dicionário                
         return cls._instances[cls]
 
 class consumo_API(metaclass=SingletonMeta):
     
-    # Parâmetros da requisição
     def __init__(self):
         self.api_key = "<sua_api_key>"
         self.lang = "pt_br"
@@ -43,7 +37,6 @@ class consumo_API(metaclass=SingletonMeta):
 
 class coleta_dados:
     
-    # Latitude e longitude do local de interesse
     def __init__(self, lat, lon):
         self.lat = lat
         self.lon = lon
@@ -59,7 +52,7 @@ class coleta_dados:
             'direcao_vento': data['wind']['deg'],
             'velocidade_vento': data['wind']['speed']
         }
-        df = pd.DataFrame(entities, index=[0])
+        df = pd.DataFrame([entities])
         print(df)
         return df
 
@@ -69,7 +62,6 @@ class armazena_mysql(metaclass=SingletonMeta):
         self.conexao = None
 
     def connect(self):
-        # Verifica se não existe uma conexão aberta
         if self.conexao is None or not self.conexao.is_connected(): 
             self.conexao = mysql.connector.connect(
                 host="<seu_host>",
@@ -82,13 +74,10 @@ class armazena_mysql(metaclass=SingletonMeta):
             print(f"Conectado ao servidor MySQL versão {self.conexao.get_server_info()}")
 
     def close(self):
-        # Verifica se existe uma conexão e se está aberta
         if self.conexao and self.conexao.is_connected():
             self.conexao.close()
 
     def armazena_dados(self, df):
-        
-        # Verifica conexão
         if not self.conexao or not self.conexao.is_connected():
             raise Exception("Não há conexão ativa com o banco de dados.")
 
@@ -123,30 +112,27 @@ class armazena_mysql(metaclass=SingletonMeta):
         print(f"{cursor.rowcount} registros inseridos na tabela.")
         cursor.close()
 
-def coleta_concluida(df, evento_conclusao_coleta):
-    armazenamento = armazena_mysql()
-    armazenamento.connect()
-    armazenamento.armazena_dados(df)
-    armazenamento.close()
-    evento_conclusao_coleta.set()
-
 def main():
     
     parser = argparse.ArgumentParser(description="Coleta e armazena dados de uma estação meteorológica com base em coordenadas geográficas.")
     parser.add_argument("--coord", nargs=2, metavar=('lat', 'lon'), required=True, help="Latitude e longitude da estação meteorológica de interesse.")
     args = parser.parse_args()
-
-    evento_conclusao_coleta = threading.Event()
     
     coletor = coleta_dados(args.coord[0], args.coord[1])
+    armazenamento = armazena_mysql()
+    armazenamento.connect()
 
-    def coleta_wrapper():
-        df = coletor.coleta_dados()
-        coleta_concluida(df, evento_conclusao_coleta)
-
-    coleta_thread = threading.Thread(target=coleta_wrapper)
-    coleta_thread.start()
-    coleta_thread.join()
+    try:
+        while True:
+            df = coletor.coleta_dados()
+            armazenamento.armazena_dados(df)
+            
+            sleep_time = random.uniform(1, 60 / 60)
+            time.sleep(sleep_time)
+    except KeyboardInterrupt:
+        print("Coleta interrompida pelo usuário.")
+    finally:
+        armazenamento.close()
 
 if __name__ == "__main__":
     main()
